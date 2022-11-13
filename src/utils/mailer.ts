a5cc9cace601fcd1fd8c50e185af278c2195bf8f
Jaideep <guntupallijaideep@gmail.com>
2022-11-13 16:23:20 +0530

    Feat: Added Custom mail service

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,26 +1,46 @@
 import nodemailer from "nodemailer";
 
-export const sendEmail = async ({email, otp}: { email: string, otp: string }) => {
+export const sendEmail = async ({
+  email,
+  otp,
+}: {
+  email: string;
+  otp: string;
+}) => {
+  let mailConfig = {};
 
-    const testAccount = await nodemailer.createTestAccount();
-
-    const transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        auth: {
-            user: testAccount.user,
-            pass: testAccount.pass,
-        },
-    });
-
-    const mailOptions = {
-        from: '"Admin" <guntupalli20378@iiitd.ac.in>',
-        to: email,
-        subject: 'OTP Email',
-        html: `<h1>Here's your OTP: ${otp}</h1>`,
+  if (process.env.NODE_ENV === "production") {
+    mailConfig = {
+      host: "smtp.mail.me.com",
+      port: 587,
+      secure: true,
+      auth: {
+        user: "no-reply-healthsake@gjd.one",
+        pass: process.env.EMAIL_PASS,
+      },
     };
+  } else {
+    const testAccount = await nodemailer.createTestAccount();
+    mailConfig = {
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: testAccount.user,
+        pass: testAccount.pass,
+      },
+    };
+  }
 
-    const mail = await transporter.sendMail(mailOptions);
+  const transporter = nodemailer.createTransport(mailConfig);
 
-    console.log("Message sent: %s", nodemailer.getTestMessageUrl(mail));
+  const mailOptions = {
+    from: '"Admin - Health Sake" <no-reply-healthsake@gjd.one>',
+    to: email,
+    subject: "OTP Email",
+    html: `<h1>Here's your OTP: ${otp}</h1>`,
+  };
+
+  const mail = await transporter.sendMail(mailOptions);
+
+  console.log("Message sent: %s", nodemailer.getTestMessageUrl(mail));
 };
